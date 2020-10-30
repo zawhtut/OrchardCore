52e37fcf672f899dee06650f9b30465a034273eb
hishamco <hishamco_2007@yahoo.com>
2020-10-30 17:47:33 +0300

    yessql.db -> OrchardCore.db

--- src/OrchardCore/OrchardCore.Data/OrchardCoreBuilderExtensions.cs
+++ src/OrchardCore/OrchardCore.Data/OrchardCoreBuilderExtensions.cs
@@ -66,7 +66,7 @@ namespace Microsoft.Extensions.DependencyInjection
                             var shellOptions = sp.GetService<IOptions<ShellOptions>>();
                             var option = shellOptions.Value;
                             var databaseFolder = Path.Combine(option.ShellsApplicationDataPath, option.ShellsContainerName, shellSettings.Name);
-                            var databaseFile = Path.Combine(databaseFolder, "yessql.db");
+                            var databaseFile = Path.Combine(databaseFolder, "OrchardCore.db");
                             Directory.CreateDirectory(databaseFolder);
                             storeConfiguration
                                 .UseSqLite($"Data Source={databaseFile};Cache=Shared", IsolationLevel.ReadUncommitted)
